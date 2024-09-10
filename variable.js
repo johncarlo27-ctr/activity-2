b3187059c4165b5250ae68c98ea012310bbc0911
unknown <johncarloaltura705@gmail.com>
2024-09-10 14:22:06 +0800

    complited variable section

---
+++ variable.js
@@ -0,0 +1,10 @@
+let name = "john carlo altura";
+let WhatDoYouWannaBecomeinYourLife = "Hacker";
+let gender = "male";
+let twitterhandle = "@jcliit27";
+
+console.log('My name is ',name);
+console.log('I wnat to become a ', WhatDoYouWannaBecomeinYourLife);
+console.log('My gender is ', gender);
+console.log('My twitter handle is ', gender);
+
